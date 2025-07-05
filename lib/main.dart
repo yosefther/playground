6a53c141ecf6e3d67971384aedc458f16c5785ef
yosefther <yousefthair9@gmail.com>
2025-07-05 23:56:53 +0300

    test how can i  downloads and installs all the dependencies listed in pubspec.yaml file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("data"))
-      )
-  );
+    home:Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text("alot of text" ,  style: GoogleFonts.roboto(fontSize: 24)  ),  
+      ) ,
+    ) ,
+  ));
 }
+
+
+/*
+
+[Flutter engine]
+└─ runApp
+   └─ MaterialApp
+      └─ Scaffold
+          └─ (empty body)
+
+ */
